ee094762cd1e5993a69c363e42e21b0b095a3975
Karthik-0704 <karthikultimate07@gmail.com>
2025-02-12 00:14:34 -0700

    Update Train

--- train.py
+++ train.py
@@ -1,8 +1,10 @@
 from ultralytics import YOLO
-import os
+import os 
+
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
 
 
 if __name__ == '__main__':
     model = YOLO("yolo11m.pt")
+    print("Training Started\n")
     model.train(data = "./data.yaml", imgsz = 640, batch = 8, epochs = 100)
